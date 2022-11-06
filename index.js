38e139fbcd65214ddac9365a9c83e06070dfa4b3
zeus69321 <117597531+zeus69321@users.noreply.github.com>
2022-11-06 11:26:58 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 let a;
 a=Math.PI;
 let n;
+let na;
+na=window.prompt("enter your name :- ");
 document.getElementById("b1").onclick=function(){
     n=document.getElementById("i1").value;
-   
     document.getElementById("p1").innerHTML= "area of circle = " +  2 * a * n + "cm2";
+    document.getElementById("h2").innerHTML="WELCOME " + na;
 }
